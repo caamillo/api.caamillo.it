465c7341aff1b839fd7ff0c0d766f16423d2cfe6
caamillo <mcamillolud@gmail.com>
2023-11-14 11:43:04 +0100

    better errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Elysia } from "elysia"
 import jwt from 'jsonwebtoken'
 import { bearer } from '@elysiajs/bearer'
+const path = require('path')
 
 import whois from "./utils/whois"
 
@@ -20,6 +21,7 @@ const auth = (token, set) => {
 
 const app = new Elysia()
   .use(bearer())
+  .get('/', () => Bun.file(path.join(import.meta.path, '../views/index.html')))
   .post('/token', async ({ body: { name, pw }, set }) => {
     if (!name || typeof name !== 'string' || !pw || typeof pw !== 'string') {
       set.status = 400
@@ -36,14 +38,14 @@ const app = new Elysia()
   })
   .group('/v1', app => {
     app.onBeforeHandle(({ bearer, set }) => {
-      if (!auth(bearer, set)) return 'Unauthorized'
+      if (!auth(bearer, set)) return Bun.file(path.join(import.meta.path, '../views/index.html'))
     })
     return app
       .get("/whois/:url", async ({ query: { parsed }, params: { url } }) =>
         await whois(url, parsed === 'true') ?? { error: 'Unexpected Error' }
       )
-  }
-  )
+  })
+  .get('/*', () => Bun.file(path.join(import.meta.path, '../views/404.html')))
   .listen(Bun.env['API_PORT']);
 
 console.log(`api.caamillo.it is running at ${ app.server?.hostname }:${ app.server?.port }`)
